046e92e5ec04f058b835537aac6e08e91b822da2
nagarajukoniki <42564531+nagarajukoniki@users.noreply.github.com>
2020-06-30 13:46:54 -0500

    Create ValidateLockedPages.java

---
+++ ValidateLockedPages.java
@@ -0,0 +1,89 @@
+package com.tda.apac.core.workflows;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.granite.workflow.WorkflowException;
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.exec.WorkflowProcess;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+@Component(name = "Validate Locked Pages -Component", service = WorkflowProcess.class, immediate = true, property = {
+		"process.label = Validate Locked Pages Process" }, configurationPid = "com.tda.apac.core.workflows.ValidateLockedPages")
+
+public class ValidateLockedPages implements WorkflowProcess {
+	protected final Logger log = LoggerFactory.getLogger(this.getClass());
+	ResourceResolver resourceResolver;
+
+	@Override
+	public void execute(WorkItem workItem, WorkflowSession wfsession, MetaDataMap metaDataMap)
+			throws WorkflowException {
+		try {
+			resourceResolver = wfsession.adaptTo(ResourceResolver.class);
+			String payload = workItem.getWorkflowData().getPayload().toString();
+			log.info("Payload ::::: " + payload);
+			String workflowPackagerPage = payload + "/jcr:content/vlt:definition/filter";
+			Resource resource = resourceResolver.getResource(workflowPackagerPage);
+			Node pageFilterNode = resource.adaptTo(Node.class);
+			if (pageFilterNode.hasNodes()) {
+				NodeIterator resourceNodes = pageFilterNode.getNodes();
+				while (resourceNodes.hasNext()) {
+					try {
+						Node childNode = (Node) resourceNodes.next();
+
+						String pagePath = childNode.hasProperty("root") ? childNode.getProperty("root").getString()
+								: "";
+						log.info("page path ::: " + pagePath);
+						if (!"".equalsIgnoreCase(pagePath)) {
+
+							validatePageLock(pagePath, wfsession, workItem);
+
+						}
+
+					} catch (Exception e) {
+
+					}
+				}
+			}
+
+		} catch (
+
+		Exception e) {
+			log.error("Exception in update workflow packager  main method  :" + e);
+		}
+	}
+
+	public void validatePageLock(String pagePath, WorkflowSession wfsession, WorkItem workItem) {
+		try {
+			PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+			if (pageManager != null) {
+				Page page = pageManager.getPage(pagePath);
+				if (page != null) {
+					boolean isPageLocked = page.isLocked();
+					if (isPageLocked) {
+						workItem.getWorkflowData().getMetaDataMap().put("ispagelocked", true);
+					} else {
+						workItem.getWorkflowData().getMetaDataMap().put("ispagelocked", false);
+						page.lock();
+						log.info(":::::page is locked");
+					}
+				} else {
+					log.info(":::::Attached asset is not a page");
+				}
+			}
+		} catch (Exception e) {
+			log.error(":::::Issue in validatepagelock method");
+		}
+
+	}
+
+}
